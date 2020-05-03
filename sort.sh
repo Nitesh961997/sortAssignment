0cb48b946c59ac310688200dac395a3d7634fb6a
Nitesh961997 <niteshkavathankar@gmail.com>
2020-05-03 12:45:48 +0530

    Second expression added to the program

--- sort.sh
+++ sort.sh
@@ -2,4 +2,5 @@
 read -p "enter the value of a" a;
 read -p "enter the value of b" b;
 read -p "enter the value of c" c;
-expresionOne=$(echo "scale=2; $a+$b*$c" | bc)
+expressionOne=$(echo "scale=2; $a+$b*$c" | bc)
+expressionTwo=$(echo "scale=2; $a*$b+$c" | bc)
